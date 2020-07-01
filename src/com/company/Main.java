9b6dd9a460e747a16e0fd1d333cb5dfc9ee625c8
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-01 09:41:17 -0700

    First Lambda expression

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        new Thread(()-> System.out.println("Printing from the Runnable")).start();
+    }
+}
+
+class CodeToRun implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Printing from runnable");
     }
 }
