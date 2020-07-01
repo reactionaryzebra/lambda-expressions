ff1ef0a227f19c7090117a1eb96926f8c56ec1b4
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-01 09:50:03 -0700

    Add basic employee class and list of employees

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,65 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        new Thread(()-> System.out.println("Printing from the Runnable")).start();
+        new Thread(()-> {
+            System.out.println("Printing from the Runnable");
+            System.out.println("line 2");
+            System.out.println("line 3");
+        }).start();
+
+        Employee justin = new Employee("Justin Reznik", 30);
+        Employee sasha = new Employee("Sasha Fierece", 36);
+        Employee marcus = new Employee("Marcus Pierson", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(justin);
+        employees.add(sasha);
+        employees.add(marcus);
+
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getName().compareToIgnoreCase(employee2.getName());
+            }
+        });
+
+        for (Employee employee : employees) {
+            System.out.println(employee.getName());
+        }
+
     }
 }
 
-class CodeToRun implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Printing from runnable");
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
