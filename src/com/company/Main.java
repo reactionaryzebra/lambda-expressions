08361147598e74d07852fc83df4eeb22679d2b51
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-01 10:06:59 -0700

    Use lambda expression with custom interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -38,6 +38,24 @@ public class Main {
             System.out.println(employee.getName());
         }
 
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        }, employees.get(0).getName(), employees.get(1).getName());
+
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+
+        String sillyString = doStringStuff(uc, justin.getName(), sasha.getName());
+
+        System.out.println(sillyString);
+
+
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -65,4 +83,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
